b62e8e14706c63b59f187a25012d2fbd02f4bbf5
linoy hadad <linoy.hadad@post.runi.ac.il>
2024-11-22 18:48:30 +0200

    finished

--- Algebra.java
+++ Algebra.java
@@ -25,43 +25,148 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if ((x1 > 0 && x2 > 0) || (x1 < 0 && x2 > 0)) {
+			while (x2 !=0) {
+				x1++;
+				x2--;
+			}
+		}
+		else if ((x1 > 0 && x2 < 0) || (x1 < 0 && x2 < 0)) {
+			while (x2 != 0) {
+				x1--;
+				x2++;
+			}
+		}
+		else if (x1 == 0) {
+			return x2;
+		}
+		else if (x2 == 0) {
+			return x1;
+		}
+		
+		return x1;
 	}
-
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x1 <= 0 && x2 < 0) {
+			while (x2 != 0) {
+				x1++;
+				x2++;
+			}
+		}
+		else if (x1 > 0 && x2 < 0){
+			while (x2!= 0){
+				x1--;
+				x2++;
+			}
+		}
+		else if ((x1 > 0 && x2 > 0) || (x1 <= 0 && x2 > 0)) {
+			while (x2 != 0) {
+				x1--;
+				x2--;
+			}
+		}
+		
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int sum = 0;
+		if (x1 > 0 && x2 > 0) {
+			while (x2 != 0) {
+			sum = plus(sum,x1) ;
+			x2--;
+			}
+		}
+		else if (x1 > 0 && x2 < 0) {
+			while (x1 != 0) {
+				sum = plus(sum, x2);
+				x1--;
+			}
+		}
+		else if (x1 < 0 && x2 > 0) {
+			while (x2 !=0 ) {
+				sum = plus (sum,x1);
+				x2--;
+			}
+		}
+		else if (x1 < 0 && x2 < 0) {
+			while (x2 != 0) {
+				sum = minus (sum,x1);
+				x2++;
+			}
+		}
+		else if (x1 == 0 || x2 == 0) {
+			return 0;
+		}
+		return sum;  
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 1;
+		if (n != 0){
+			while (n != 0) {
+				result = times (result,x);
+			}
+		}
+		else return 1;
+		
+		return result;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x1 == 0 || x2==0) {
+			return -1;
+		 }
+		 int newx1 = x1;
+		 int newx2 = x2;
+		 if (x1 < 0) {
+			newx1 = times (x1,-1);
+		 }
+		 if (x2 < 0) {
+			newx2 = times (-1,x2);
+		 }
+		 if (newx2 > newx1) {
+			return 0;
+		 }
+		 int index = 0;
+		 int result = 0;
+		 while (result < newx1) {
+			index++;
+			result = times (newx2,index);
+			
+		 }
+
+		 if ((x1 > 0 && x2 < 0) || (x1 < 0 && x2 > 0)) {
+			return times (-1,minus(index,1));
+		 }
+			
+		 return minus(index,1);
+
+		 }
+		
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		
+		return minus(x1,times(x2,div(x1,x2)));
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		if (x < 0){
+			return 0;
+		}
+		int index = 0;
+		int result = 0;
+		while (index != div(x,index)) {
+			index++;
+			result = div(x,index);
+		}
+		return index;
 	}	  	  
 }
