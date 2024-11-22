86e416276e474654bacafb35065bdd1b5b306196
linoy hadad <linoy.hadad@post.runi.ac.il>
2024-11-22 19:44:52 +0200

    finished

--- Algebra.java
+++ Algebra.java
@@ -6,21 +6,8 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
+	    System.out.println(times(-2, -3));
+	    
 	}  
 
 	// Returns x1 + x2
@@ -93,7 +80,7 @@ public class Algebra {
 		}
 		else if (x1 < 0 && x2 < 0) {
 			while (x2 != 0) {
-				sum = minus (sum,x1);
+				sum = plus(sum,minus (0,x1));
 				x2++;
 			}
 		}
